608206fc659dd08c80638b99f64da2e2d976608d
Daniela Bustamante <96563765+DanielaBustamante@users.noreply.github.com>
2022-01-12 15:50:16 -0300

    Add files via upload

---
+++ 3 - Volumen y centro de gravedad.m
@@ -0,0 +1,101 @@
+﻿%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%% CALCULO II %%%%%%%%%%%%%%%
+%%%% TP1: VOLUMEN-CENTRO DE GRAVEDAD %%%%
+%%%%%%% ALUMNO: BUSTAMANTE DANIELA %%%%%%%
+%%%%%%%%%%%%% DNI: 44.093.292 %%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% z=3x^2+3y^2, z=16-x^2-y^2, funcion densidad f(x,y,z)=x^2+y^2
+
+% 1. Hallar el volumen del solido
+
+clear, clc
+
+disp('Calculamos el volumen del solido limitado por:')
+
+disp('los conos z=3x^2+3y^2 y z=16-x^2-y^2')
+
+disp('Para realizar el calculo utilizaremos coordenadas cilindricas')
+
+disp('Proyectando sobre el plano xy, nos quedan los siguientes limites')
+
+disp('0<r<2')
+
+disp('0<t<2*pi')
+
+disp('3*r^2<z<16-r^2')
+
+
+disp('Calculamos la integral')
+
+syms r t z 
+
+int1=int(r,z,3*r^2,16-r^2);
+
+int2=int(int1,t,0,2*pi);
+
+Vol=int(int2,r,0,2);
+
+disp('El volumen es')
+
+disp(Vol)
+
+% 2. Calcular su masa y su centro de gravedad
+
+disp('Si la densidad del solido es f(x,y,z)=x^2+y^2')
+
+disp('Calculamos la masa del solido')
+
+int1=int(r*r^2,z,3*r^2,16-r^2);
+
+int2=int(int1,t,0,2*pi);
+
+Masa=int(int2,r,0,2);
+
+disp('La masa es')
+
+disp(Masa)
+
+Xg=int(int(int(r^4*cos(t),z,3*r^2,16-r^2),t,0,2*pi),r,0,2)/Masa;
+
+Yg=int(int(int(r^4*sin(t),z,3*r^2,16-r^2),t,0,2*pi),r,0,2)/Masa;
+
+Zg=int(int(int(r^3*z,z,3*r^2,16-r^2),t,0,2*pi),r,0,2)/Masa;
+
+G=[Xg Yg Zg];
+
+disp('El centro de gravedad se encuentra en:')
+
+disp(G )
+
+% 3. Graficar el solido y su centro de gravedad
+
+disp('Graficaremos el solido y el centro de gravedad')
+
+r=linspace(0,2,30);
+
+t=linspace(0,2*pi,30);
+
+[R,T]=meshgrid(r,t);
+
+X=R.*cos(T);
+
+Y=R.*sin(T);
+
+Z=3.*X.^2+3.*Y.^2;
+
+surf(X,Y,Z,'FaceAlpha',0.2)
+
+hold on %autoriza la otra grafica
+
+Z=16-X.^2-Y.^2;
+
+surf(X,Y,Z,'FaceAlpha',0.2)
+
+axis equal
+
+hold on
+
+plot3(Xg,Yg,Zg,'. b','markersize',30)
+
+title('Volumen y centro de gravedad')
---
+++ 4 - Teorema de Gauss.m
@@ -0,0 +1,130 @@
+﻿
+%Vectores posición iniciales; [m] 
+ro1o=[0,0,1];                          %absoluto O1 respecto de O
+ro2a=[-0.2,1.5,2];                     %relativo O2 respecto de A
+rpb=[-0.1,1.9,1.8];                    %relativo p respecto de B
+
+%----------------------------------------------------------------------------
+
+%Angulos de giro dato [°]
+ang=[20, 40, 38, 10]; 
+r=pi/180; t=1; % dt [s]
+%
+%Angulos de giro convertidos [rad] - TERNA [A] respecto de O
+%
+psia=ang(1)*r; thea=0; phia=ang(2)*r;  %ángulos psi, theta, phi de TERNA [A]
+a=[psia, thea, phia];                  %vector auxiliar - TERNA [A]
+%
+%Angulos de giro convertidos [rad] - TERNA [B] respecto de A
+%
+psib=ang(3)*r; theb=0; phib=ang(4)*r; %ángulos psi, theta, phi de TERNA [B]
+b=[psib, theb, phib];                  %vector auxiliar - TERNA [B]
+
+%--------------------------------------------------------------------
+
+%Matriz de rotación - TERNA [A]
+R_za=[cosd(20) -sind(20) 0; 
+      sind(20) cosd(20) 0;
+      0 0 1];
+     
+R_ya=[1 0 0; 
+      0 1 0;
+      0 0 1]; 
+      
+R_xa=[1 0 0; 
+      0 cosd(40) -sind(40);
+      0 sind(40) cosd(40)];
+
+%--------------------------------------------------------------------
+
+Rao=R_za*R_ya*R_xa;     %Matriz total rotación - TERNA [A] respecto de O
+disp('1_ Matriz de rotacion terna A respecto de O')
+disp(Rao)
+
+%--------------------------------------------------------------------
+
+%Matriz de rotación - TERNA [B]
+R_zb=[cosd(38) -sind(38) 0; 
+      sind(38) cosd(38) 0;
+      0 0 1 ];
+      
+R_yb=[1 0 0; 
+      0 1 0;
+      0 0 1];
+      
+R_xb=[1 0 0; 
+      0 cosd(10) -sind(10);
+      0 sind(10) cosd(10)];
+
+%--------------------------------------------------------------------
+
+Rba=R_zb*R_yb*R_xb;     %Matriz total rotación - TERNA [B] respecto de A
+disp('_Matriz de rotacion terna B respecto de A')
+disp(Rba)
+
+%--------------------------------------------------------------------  
+
+%Matriz velocidad angular - TERNA [A] respecto de O
+
+psiap=psia/t; theap=0; phiap=phia/t;   
+
+omao=[0 -psiap 0; 
+      psiap 0 -phiap;
+      0 phiap 0];
+      
+disp('4_ velocidad angular terna A respecto de O')
+disp(omao) 
+
+%--------------------------------------------------------------------      
+
+%Matriz velocidad angular - TERNA [B] respecto de A
+
+psibp=psib/t; thebp=0; phibp=phib/t;   
+
+omba=[0 -psibp 0; 
+      psibp 0 -phibp;
+      0 phibp 0];
+      
+disp('_velocidad angular terna B respecto de A')
+disp(omba)
+
+%-------------------------------------------------------------------- 
+
+disp('3_Matriz de posicion de punto P respeto de terna fija')
+
+Ro1o=[0
+      0
+      1]; 
+
+Ro2a=[-0.2
+       1.5
+       1]; 
+       
+Rpb=[0.1
+     0.4
+     -0.2];       
+
+rpo=(Ro1o+Rao*Ro2a+Rao*Rba*Rpb) 
+
+%--------------------------------------------------------------------
+
+%Vector velocidad lineal de punto p respecto de terna fija; [m/s]
+
+disp('5_ vector velocidad lineal del punto p respecto terna fija')
+
+vpo=(cross(omao,Rao)*Ro2a+cross(omao,Rao)*Rba*Rpb+Rao*cross(omba,Rba)*Rpb) 
+
+%cross=producto vectorial
+
+%--------------------------------------------------------------------
+
+disp('2_ vector de posicion relativa O2A')
+vo2a=[-0.2;
+      1.5;
+      1];
+disp(vo2a)
+disp('vector posición relativa P A')
+vpa=[0.1;
+     0.4;
+    -0.2];
+disp(vpa)
